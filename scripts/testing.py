05e6426f5a9948f609a474f2cd861bf089fa5c9d
David Rein <irving.rein@duke.edu>
2018-04-20 18:02:12 -0400

    counts of ips

--- scripts/testing.py
+++ scripts/testing.py
@@ -14,20 +14,56 @@ import dask.dataframe as dd
 
 
 # preset datatypes
-dtypes = {
-        'ip'            : 'uint32',
-        'app'           : 'uint16',
-        'device'        : 'uint16',
-        'os'            : 'uint16',
-        'channel'       : 'uint16',
-        'is_attributed' : 'uint8',
-        }
+dtypesFull = {
+	'ip'            : 'uint32',
+	'app'           : 'uint16',
+	'device'        : 'uint16',
+	'os'            : 'uint16',
+	'channel'       : 'uint16',
+	'click_time'	: 'uint32',
+	'is_attributed' : 'uint8'
+}
 
-columns = ['ip', 'app', 'device', 'os', 'channel', 'click_time', 'is_attributed']
+dtypes = {
+	'ip' : 'uint32',
+	'click_time' : 'uint32',
+	'is_attributed' : 'uint8'
+}
+
+columns = ['ip', 'click_time', 'is_attributed']
+
+ips_df = pd.read_csv("../train/train_cleaned.csv", usecols = columns, dtype = dtypes)
+
+print(ips_df.info())
+ips_df.head()
+
+size=100000
+all_rows = len(ips_df)
+num_parts = all_rows//size
+
+#generate the first batch
+ip_counts = ips_df[0:size][['ip', 'is_attributed']].groupby('ip', as_index=False).count()
+
+#add remaining batches
+for p in range(1,num_parts):
+    start = p*size
+    end = p*size + size
+    if end < all_rows:
+        group = ips_df[start:end][['ip', 'is_attributed']].groupby('ip', as_index=False).count()
+    else:
+        group = ips_df[start:][['ip', 'is_attributed']].groupby('ip', as_index=False).count()
+    ip_counts = ip_counts.merge(group, on='ip', how='outer')
+    ip_counts.columns = ['ip', 'count1','count2']
+    ip_counts['counts'] = np.nansum((ip_counts['count1'], ip_counts['count2']), axis = 0)
+    ip_counts.drop(columns=['count1', 'count2'], axis = 0, inplace=True)
+
+ip_counts.sort_values('counts', ascending=False)[:20]
+
+sys.exit()
 
 # with more training data,
 # we will chunk the csv and do this section by section for memory
-train = pd.read_csv("../train_sample/train_sample.csv", usecols = columns, dtype = dtypes)
+train = pd.read_csv("../train/train_sample.csv", dtype = dtypesFull)
 train['click_time'] = pd.to_datetime(train['click_time']).dt.round('H')
 train['click_time'] = (train['click_time'] - train['click_time'].min()) / np.timedelta64(1, 'h')
 print("Done Parsing\n")
@@ -69,19 +105,19 @@ with open("../predictions/predictions.csv", "w", newline = '') as writeCSV:
 	# create initTime array
 	for i in range(0, chunkSize):
 		initTime.append(t)
-	for chunk in reader:
-		chunkNumber += 1
-		print(chunk.iloc[0].tolist())
+		for chunk in reader:
+			chunkNumber += 1
+			print(chunk.iloc[0].tolist())
 
-		dt = chunk.iloc[:,6].tolist()
-		for i in range(0, len(dt)):
-			dt[i] = parser.parse(dt[i])
-		dt = list(map(operator.sub, dt, initTime))
-		dt = list(map(timedelta.total_seconds, dt))
-		chunk['click_time'] = dt
-		ids = chunk.pop('click_id').tolist()
-		X = chunk.as_matrix()
-		probs = clf.predict_proba(X)
+			dt = chunk.iloc[:,6].tolist()
+			for i in range(0, len(dt)):
+				dt[i] = parser.parse(dt[i])
+				dt = list(map(operator.sub, dt, initTime))
+				dt = list(map(timedelta.total_seconds, dt))
+				chunk['click_time'] = dt
+				ids = chunk.pop('click_id').tolist()
+				X = chunk.as_matrix()
+				probs = clf.predict_proba(X)
 
 		# write to csv
 		output = []
@@ -89,8 +125,8 @@ with open("../predictions/predictions.csv", "w", newline = '') as writeCSV:
 			line = [int(ids[i]), probs[i][1]]
 			if (line[1] > 0.5):
 				print(line)
-			output.append(line)
-		for row in output:
-			writer.writerow([row[0], row[1]])
+				output.append(line)
+				for row in output:
+					writer.writerow([row[0], row[1]])
 
-		print("Done with chunk", chunkNumber, "\n")
+					print("Done with chunk", chunkNumber, "\n")
